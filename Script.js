173c701db6decf13bbb8233144a10f313cb0f08c
angadtewari09 <despacito0099@gmail.com>
2022-06-10 23:40:23 +0530

    Initial Commit

--- Script.js
+++ Script.js
@@ -2,23 +2,8 @@ console.log('hello');
 console.log('world')
 
 document.getElementById('button').addEventListener('click' , addItem);
-// function showPoster() {
-//     console.log('function running');
-//     const element = document.createElement('div');
-//     element.classList.add('image');
-//     element.innerHTML = `
-//     <img class="image" src="600x600-2_p1g3e4vs7t8lq1uag1q1l1sc61dd03.jpg">
-//    `
-//    document.body.appendChild(element);
-//}
 
-// function showPoster() {
-//     var img = document.createElement('img');
-//     img.src="600x600-2_p1g3e4vs7t8lq1uag1q1l1sc61dd03.jpg"
-//     img.classList.add('image');
-//     document.getElementById('main-body').appendChild(img);
-//     console.log("Image tag added!");
-// }
+
 function addItem() {
     var chunk = document.createElement('div');
     chunk.classList.add('chunk');
@@ -30,7 +15,7 @@ function addItem() {
     var overlay = document.createElement('div');
     overlay.classList.add('overlay')
 
-    var content = document.createElement('content');
+    var content = document.createElement('div');
     content.classList.add('content');
     content.innerHTML = `Friday Brands`;
 
@@ -39,10 +24,73 @@ function addItem() {
     document.getElementById('main-body').appendChild(chunk);
     chunk.appendChild(img);
     chunk.appendChild(overlay);
+    
+    const model = document.createElement('div');
+    model.classList.add('modal')
+    model.setAttribute("id" , "myModal");
+
+    var model_content = document.createElement('div')
+    model_content.classList.add('modal_content');
+
+    var top = document.createElement('div')
+    top.classList.add('top');
+
+    var p = document.createElement('p')
+    p.classList.add("content_title")
+    p.innerHTML = `Title of the project!`
+    console.log(p.innerHTML)
+
+    var span = document.createElement('span');
+    span.classList.add('close');
+    span.innerHTML = `&times;`
+    console.log(span.innerHTML)
+
+    model_content.appendChild(top);
+    top.appendChild(p)
+    top.appendChild(span)
+
+    model.innerHTML = `<div class="modal-content">
+    <div class="top">
+        <p class="content_title">Heeloo this is modal! </p>
+        <span class="close">&times;</span>
+    </div>
+    <div class="content_description">
+        <span class="content_leftside">Lorem ipsum dolor sit amet 
+            consectetur adipisicing elit. Alias ab sit ipsa, consequatur 
+            voluptas itaque culpa perferendis non tempore quam.</span>
+        <div class="content_rightside">
+            <span class="date">
+                Date: 21st November 2021 
+            </span>
+            <br>
+            <span class="client">
+                Client: Friay Brands
+            </span>
+            <br>
+            <span class="role">
+                Role:  Designer
+            </span>
+            <br>
+            <span class="url">
+                URL: <a href="#" >brandsbyfriday.com</a>
+            </span>
+            <br>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="portfolio_images">
+        <img class="images" src="Cyberpunk-4K-Wallpaper.jpg">
+        <img class="images" src="space_planet_surface_shadow_46631_3840x2160.jpg">
+        <img class="images" src="4k-desktop-wallpaper.-2560×1440.jpg">
+    </div>
+</div>`
+document.getElementById('main-body').appendChild(model);
 }
 
 //Open and close Modal
 var modal = document.getElementById("myModal");
+var modalContent = document.querySelector("modal_content")
 var body = document.querySelector("body");
 
 // Get the button that opens the modal
@@ -55,6 +103,8 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
   body.style.overflowY = "hidden";
+//   modalContent.classList.add("animate")
+
 }
 
 // When the user clicks on <span> (x), close the modal
